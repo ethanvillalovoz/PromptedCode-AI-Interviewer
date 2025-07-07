2385277caf2b9a4ee65c876ef4a7ad9c25a7378b
ethanvillalovoz <ethan.villalovoz@gmail.com>
2025-07-07 05:38:14 -0700

    Implemented AuthenticationPage

--- frontend/src/auth/AuthenticationPage.jsx
+++ frontend/src/auth/AuthenticationPage.jsx
@@ -1,5 +1,18 @@
 import "react"
+import { SignIn, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react"
 
 export function AuthenticationPage() {
-  return <></>
+  return (
+    <div className="auth-container">
+      <SignedOut>
+        <SignIn path="/sign-in" routing="path"/>
+        <SignUp path="/sign-up" routing="path" />
+      </SignedOut>
+      <SignedIn>
+        <div className="redirect-message">
+          <p>You are signed in!</p>
+        </div>
+      </SignedIn>
+    </div>
+  )
 }
